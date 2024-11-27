f40a795ecbdbcfadde3522bddef7019dac7b61b5
Christoph Cernusca <cernusca.c22@htlwienwest.at>
2024-11-27 21:46:13 +0100

    Interface update - Added user alias - Reworked message formatting - Reworked message displaying

--- yell.py
+++ yell.py
@@ -1,12 +1,36 @@
 import socket
 import threading
 import time
+import json
 
 # Port number the application is working on (listening & sending)
 APP_PORT = 12345
 
 # Global variable for storing messages waiting to be displayed
 message_queue = []
+
+# Global variable for storing user alias
+alias = None
+
+def init_alias():
+    """
+    Initialize the global user alias variable by asking the user for it. The
+    alias is stored in a global variable and can be accessed from any function.
+    The alias is stripped of leading and trailing whitespace.
+    """
+    global alias
+    alias = input("Enter your alias: ").strip()
+
+def format_message(message, adress):
+    """
+    Return a formatted string for displaying a message in the chat window.
+    The message string includes the sender's alias and the sender's address.
+
+    :param message: A dictionary containing the message's 'alias' and 'text'.
+    :param adress:  The sender's address as a string.
+    :return: A string representation of the message.
+    """
+    return f"{message['alias']} ({adress[0] + ':' + str(adress[1])}) : {message['text']}"
 
 def start_listener():
     """
@@ -24,12 +48,11 @@ def start_listener():
         """
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
             server_socket.bind(("", APP_PORT))  # Listening on all interfaces
-            print(f"Server listening on 0.0.0.0:{APP_PORT}")
 
             while True:
                 data, address = server_socket.recvfrom(1024)
                 global message_queue
-                message_queue.append(f"Received '{data.decode()}' from {address}")  # Add message to queue to be displayed on update
+                message_queue.append(format_message(json.loads(data.decode()), address))  # Add message to queue to be displayed on update
 
     listener_thread = threading.Thread(target=listen, daemon=True)
     listener_thread.start()
@@ -41,7 +64,7 @@ def send(message):
     """
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(message.encode(), ("255.255.255.255", APP_PORT))  # Broadcast message on APP_PORT
+        sock.sendto(json.dumps(message).encode('utf-8'), ("255.255.255.255", APP_PORT))  # Broadcast message on APP_PORT
 
 def start_yeller():
     """
@@ -51,11 +74,16 @@ def start_yeller():
     update.
     """
     while True:
-        message = input("Send message: ").strip()
+        text = input("Send message: ").strip()
         print("\033[F\033[K", end="", flush=True)  # Delete old input query by moving cursor up and clearing line
 
+        message = {
+            "alias": alias,
+            "text": text
+            }
+
         # Only send messages with content, therefore allowing display updating using empty messages
-        if message != "":
+        if message["text"] != "":
             send(message)
 
         # Update dispayed messages
@@ -63,6 +91,8 @@ def start_yeller():
         message_queue.clear()
 
 if __name__ == "__main__":
+    init_alias()
+
     start_listener()
 
     # Wait for the listener to start
